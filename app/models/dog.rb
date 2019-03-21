c3b16dfc1e9e486d0c444ce433a06e5ae4da983c
Eric Eberhart <el.eberhart40@gmail.com>
2019-03-21 09:43:17 -0400

    Add user seeds

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,6 @@
 class Dog < ApplicationRecord
   has_many_attached :images
+
+  belongs_to :owner,
+  class_name: :User
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,52 +5,77 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
+Dog.delete_all
+
+dave = User.create!({name: "David Wood", email: "woody@gmail.com"})
+lynn = User.create!({name: "Aunt Lynn", email: "lynnHana@gmail.com"})
 
 dogs = [
   {
     name: 'Bowie',
-    description: 'Bowie dances when he walks'
+    description: 'Bowie dances when he walks',
+    user_id: dave.id
   },
   {
     name: 'Brownie',
-    description: 'Brownie only sits pretty'
+    description: 'Brownie only sits pretty',
+    user_id: lynn.id
   },
   {
     name: 'Jax',
     description: '',
+    user_id: dave.id
+
   },
   {
     name: 'Jiro',
-    description: 'Jiro takes a long time to destroy his toys'
+    description: 'Jiro takes a long time to destroy his toys',
+    user_id: lynn.id
+
   },
   {
     name: 'Pete',
-    description: 'Pete has a best friend named Lua'
+    description: 'Pete has a best friend named Lua',
+    user_id: dave.id
+
   },
   {
     name: 'Bijou',
-    description: 'Bijou is the fluffiest of them all'
+    description: 'Bijou is the fluffiest of them all',
+    user_id: lynn.id
+
   },
     {
     name: 'Britta',
-    description: 'Britta has two different colored eyes'
+    description: 'Britta has two different colored eyes',
+    user_id: lynn.id
+
   },
   {
     name: 'Noodle',
-    description: 'Noodle is an Instagram celebrity'
+    description: 'Noodle is an Instagram celebrity',
+    user_id: dave.id
+
   },
   {
     name: 'Stella',
-    description: 'Stella loves to dig'
+    description: 'Stella loves to dig',
+    user_id: lynn.id
+
   },
   {
     name: 'Tonks',
-    description: 'Tonks loves to run'
+    description: 'Tonks loves to run',
+    user_id: dave.id
+
   },
 ]
 
+
+
 dogs.each do |dog|
-  dog = Dog.find_or_create_by(name: dog[:name], description: dog[:description])
+  dog = Dog.find_or_create_by(name: dog[:name], description: dog[:description], user_id: dog[:user_id])
   directory_name = File.join(Rails.root, 'db', 'seed', "#{dog[:name].downcase}", "*")
 
   Dir.glob(directory_name).each do |filename|
